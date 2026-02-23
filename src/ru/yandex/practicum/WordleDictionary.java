26a2bcfbcc1b35e8db2a27dff7b703e34f90f36f
Егор <volyirazum144@yandex.ru>
2026-02-23 20:40:05 +0800

    FIX: Error test GitHub

--- src/ru/yandex/practicum/WordleDictionary.java
+++ src/ru/yandex/practicum/WordleDictionary.java
@@ -16,7 +16,6 @@ public class WordleDictionary {
     private HashSet<String> wordSet;
     private PrintWriter log;
 
-
     Random random = new Random();
 
     public WordleDictionary(List<String> words, PrintWriter log) {
