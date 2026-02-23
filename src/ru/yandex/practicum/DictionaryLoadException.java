48851fb77dcc5a1c0491c379f4aba923e659dc19
Егор <volyirazum144@yandex.ru>
2026-02-23 20:36:24 +0800

    FIX: Error test GitHub

--- src/ru/yandex/practicum/DictionaryLoadException.java
+++ src/ru/yandex/practicum/DictionaryLoadException.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum;
 
 public class DictionaryLoadException extends Exception {
+
     public DictionaryLoadException(String message) {
         super(message);
     }
--- src/ru/yandex/practicum/GameOverException.java
+++ src/ru/yandex/practicum/GameOverException.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum;
 
 public class GameOverException extends GameException {
+
     public GameOverException(String message) {
         super(message);
     }
